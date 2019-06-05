8f9f899b02c9fec5a6a7a0b63c2fc43f65060e6a
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-06-05 13:56:16 -0700

    add comments for code

--- server/service.js
+++ server/service.js
@@ -1,6 +1,10 @@
+//With strict mode, you can not, for example, use undeclared variables.
 'use strict';
 
+// setting up the express server
 const express = require('express');
 const service = express();
 
+
+//exporting the service
 module.exports = service;
