3dc080546e2fc9e78c431957723e7ad50d396aa5
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-06-07 15:09:07 -0700

    move code from run.js to slackClient

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,43 +1,52 @@
-// 'use strict';
+'use strict';
 
-// const { RTMClient } = require('@slack/rtm-api');
-// let rtm = null;
+const { RTMClient } = require('@slack/rtm-api');
+let rtm = null;
+let nlp = null;
 
-// function handleOnAuthenticated(rtmStartData){
-//     // // After the connection is open, your app will start receiving other events.
-//     // rtm.on('user_typing', (event) => {
-//     //   // The argument is the event as shown in the reference docs.
-//     //   // For example, https://api.slack.com/events/user_typing
-//     //   console.log(event);
-//     // })
-//     console.log(`Logged in as ${rtmStartData.self.name} on team ${rtmStartData.team.name}, but not yet connected to a channel`);
-// }
 
-// function addAuthenticatedHandler(rtm, handler){
-//     rtm.on('authenticated', (handler) => {
-//     console.log(handler);
-//     })
-// }
 
-// function handleOnMessage(message){
-//   // Sending a message requires a channel ID, a DM ID, an MPDM ID, or a group ID
-//   // The following value is used as an example
-//   const conversationId = 'CHLQ1HU91';
+module.exports.init = function slackClient(token,logLevel, nlpClient){
+    // The client is initialized and then started to get an active connection to the platform
+    rtm = new RTMClient(token,{logLevel: logLevel});
+    nlp = nlpClient;
+        
+    rtm.on('authenticated', (event) => {
+    // The argument is the event as shown in the reference docs.
+    // For example, https://api.slack.com/events/user_typing
+    console.log(event);
+    console.log(`Logged in as ${event.self.name} on team ${event.team.name}, but not yet connected to a channel`)
+    })
 
-//   // The RTM client can send simple string messages
-//   const res = await rtm.sendMessage('Hello there team', conversationId);
+    // // Calling `rtm.on(eventName, eventHandler)` allows you to handle events (see: https://api.slack.com/events)
+    // When the connection is active, the 'ready' event will be triggered
+    rtm.on('ready', async () => {
 
-//   // `res` contains information about the sent message
-//   console.log('Message sent: ', res.ts);
-//     console.log(message);
-// }
+    // Sending a message requires a channel ID, a DM ID, an MPDM ID, or a group ID
+    // The following value is used as an example
+    const conversationId = 'CHLQ1HU91';
 
-// module.exports.init = function slackClient(token,logLevel){
-//     // The client is initialized and then started to get an active connection to the platform
-//     rtm = new RTMClient(token,{logLevel: logLevel});
-//     addAuthenticatedHandler(rtm, handleOnAuthenticated);
-//     rtm.on('messsage', handleOnMessage );
-//     return rtm;
-// }
+    // The RTM client can send simple string messages
+    const res = await rtm.sendMessage('New message', conversationId);
 
-// module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
+    // `res` contains information about the sent message
+    console.log('Message sent: ', res.ts);
+    });
+
+    // After the connection is open, your app will start receiving other events.
+    rtm.on('user_typing', (event) => {
+    // The argument is the event as shown in the reference docs.
+    // For example, https://api.slack.com/events/user_typing
+    console.log(event);
+    })
+
+    rtm.on('message', (event) => {
+        nlp.ask(event.text);
+        // The argument is the event as shown in the reference docs.
+        // For example, https://api.slack.com/events/user_typing
+        rtm.sendMessage('this is a test message', 'CHLQ1HU91', function messageSent(){
+        });
+        console.log(event);
+    })
+    return rtm;
+}
