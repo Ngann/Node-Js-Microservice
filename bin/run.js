6ac2292c10ea4c1969f985acc6d656543683d51f
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-06-09 11:44:36 -0700

    add service annoucement every 15 sec

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const request = require('superagent');
 //import the service from service.js
 const service = require('../server/service');
 
@@ -13,10 +14,10 @@ const slackClient = require('../server/slackClient');
 // slackClient.addAuthenticatedHandler(rtm,() => server.listen(3000))
 // An access token (from your Slack app or custom integration - usually xoxb)
 // const token = process.env.SLACK_TOKEN;
-const slackToken ='xoxb-609731003684-645236093250-l82ByGnNRNFxBCTwg0RAmAyA';
+const slackToken =process.env.SLACK_TOKEN;
 const slackLogLevel = 'verbose';
 
-const witToken = 'KPMFSP47OMKLHSFAQWZXUJI3VTSHSIHV';
+const witToken = process.env.WIT_TOKEN;
 const witClient = require('../server/witClient')(witToken);
 
 const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
@@ -29,4 +30,19 @@ server.listen(3000);
 // server.address().port will returen object information about the running server
 server.on('listening', function(){
     console.log(`IRIS is listning on ${server.address().port} in ${service.get('env')}.mode`)
+
+    const announce = () => {
+        request.put(`http://localhost:3000/service/time/${server.address().port}`, (err, res) =>{
+            if(err){
+                console.log(err)
+                console.log("Error connecting to Iris");
+                return;
+            }
+            console.log(res.body);
+        })
+    };
+
+    //tell Iris that we are still alive, so we'll add set interval announce, and we will do this every 15 times thousand milliseconds, which means 15 seconds.
+    announce();
+    setInterval(announce, 15*1000);
 })
--- server/service.js
+++ server/service.js
@@ -5,6 +5,26 @@
 const express = require('express');
 const service = express();
 
+// make microservice resilience -- add flexibity and ability to recover service registry
+// the service need to know the endpoint of the main application 
+// below is the service registry
+
+//service.get('/service/:intent/:port', (req, res, next) =>{
+service.put('/service/:intent/:port', (req, res, next) =>{
+    const serviceIntent = req.params.intent;
+    const servicePort = req.params.port;
+
+    // check if it is an IPv6 address check if it inclues ('::') if it does we use a ternary syntax ? ``, if it is a IPv6 we need to put it in curly brakctes
+    const serviceIP = req.connection.remoteAddress.includes('::')
+    ?`[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+    res.json({result: `${serviceIntent} at ${serviceIP}:${servicePort}`})
+
+});
 
 //exporting the service
 module.exports = service;
+
+// change service to get to check if service is working, result is like below:
+//http://localhost:3000/service/time/3010
+//{"result":"time at [::1]:3010"}
